c8bf557998beac01f664a3411c0d668eee5757ce
Huda <hudaayaz21@gmail.com>
2024-12-12 19:12:20 -0500

    Set up site

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to My Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
